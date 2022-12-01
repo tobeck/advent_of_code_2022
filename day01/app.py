9e40810f65e3246c1ca8a8a43d8357e6d024187e
tobeck <tobias.becker.olsson@gmail.com>
2022-12-01 16:19:37 +0100

    Day01 part2 sum of top three

--- day01/app.py
+++ day01/app.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 
-def most_calories_carried(inventory_lines):
-    """most_calories_carried sums up the amount of calories each elf is carrying
-    and return the sum of the one who carries the most, most_calories_carried"""
+def calculate_elf_inventory(inventory_lines):
+    """calculate_max_calories sums up the amount of calories each elf is carrying
+    and return the sum of the one who carries the most, max_calories"""
 
     # List of how many calories each elf is carrying.
-    elf_calories = []
+    sum_calories_list = []
     # The sum of calories that an elf is carrying.
-    elf_sum = 0
+    sum = 0
 
     for line in inventory_lines:
         # If there is a newline, a new elf needs to be summed up.
         if line.strip() == "":
-            elf_calories.append(elf_sum)
-            elf_sum = 0
+            sum_calories_list.append(sum)
+            sum = 0
         else:
-            elf_sum += int(line)
+            sum += int(line)
 
-    most_calories_carried = max(elf_calories)
-
-    return most_calories_carried
+    return sum_calories_list
 
 
 def main():
@@ -28,7 +26,13 @@ def main():
     with open('elf_inventory.txt', mode='r', encoding='utf-8') as file:
         inventory_lines = file.readlines()
 
-    print(most_calories_carried(inventory_lines=inventory_lines))
+    sum_calories_list = calculate_elf_inventory(
+        inventory_lines=inventory_lines)
+
+    sum_calories_list.sort()
+
+    print("The most calories carried is:", max(sum_calories_list))
+    print("Top three elfs carry:", sum(sum_calories_list[-3:]))
 
 
 if __name__ == "__main__":
