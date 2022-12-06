f36b7b1e8313f7ae786b6cca4a3a3df2405578f3
tobeck <tobias.becker.olsson@gmail.com>
2022-12-06 09:39:23 +0100

    Day04 Part 1 pair_in_other test

---
+++ day04/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def main():
+    pass
+
+
+def pair_in_other():
+    pass
+
+
+def test_pair_in_other():
+    assert pair_in_other('2-8', '3-7') == True
+
+
+if __name__ == "__main__":
+    main()
