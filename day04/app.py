873cb2993b49ea6eb5b352fba8d0dbdfca481532
tobeck <tobias.becker.olsson@gmail.com>
2022-12-06 13:26:22 +0100

    Day04 part 2 added input method issubset or intersection

--- day04/app.py
+++ day04/app.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
 
-def main():
-    with open('test_data.txt', mode='r', encoding='utf-8') as file:
-        number_of_subsets = 0
-
-        for line in file:
-            section_1 = line.split(',')[0]
-            section_2 = line.split(',')[1]
-            if pair_in_other(section_1, section_2) == True:
-                number_of_subsets += 1
-
-        print(number_of_subsets)
-        return number_of_subsets
-
-
-def pair_in_other(section_1, section_2):
+def pair_in_other(section_1, section_2, method):
     range_1_split = [int(i) for i in section_1.split('-')]
     range_1_set = set(i for i in range(
         range_1_split[0], range_1_split[1] + 1))
@@ -23,18 +9,44 @@ def pair_in_other(section_1, section_2):
     range_2_set = set(i for i in range(
         range_2_split[0], range_2_split[1] + 1))
 
-    return range_1_set.issubset(range_2_set) or range_2_set.issubset(range_1_set)
+    if method == 'issubset':
+        result = range_1_set.issubset(
+            range_2_set) or range_2_set.issubset(range_1_set)
+        print(result)
+    elif method == 'intersection':
+        if range_1_set.intersection(
+                range_2_set) or range_2_set.intersection(range_1_set) == True:
+            result = True
+        else:
+            result = False
+
+    return result
+
+
+def main(method):
+    with open('test_data.txt', mode='r', encoding='utf-8') as file:
+        number_of_subsets = 0
+
+        for line in file:
+            section_1 = line.split(',')[0]
+            section_2 = line.split(',')[1]
+            if pair_in_other(section_1, section_2, method=method) == True:
+                number_of_subsets += 1
+
+        print(number_of_subsets)
+        return number_of_subsets
 
 
 def test_main():
-    assert main() == 2
+    assert main(method='issubset') == 2
+    assert main(method='intersection') == 4
 
 
 def test_pair_in_other():
-    assert pair_in_other('2-4', '6-8') == False
-    assert pair_in_other('2-8', '3-7') == True
-    assert pair_in_other('2-6', '4-8') == False
+    assert pair_in_other('2-4', '6-8', 'issubset') == False
+    assert pair_in_other('2-8', '3-7', 'issubset') == True
+    assert pair_in_other('2-6', '4-8', 'issubset') == False
 
 
 if __name__ == "__main__":
-    main()
+    main(method='intersection')
