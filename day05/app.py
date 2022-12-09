97abfbfb5b7504bb77ba6bdc11b805126f301016
tobeck <tobias.becker.olsson@gmail.com>
2022-12-09 14:32:07 +0100

    Day05 problem 1 stuck at moving multiple boxes

--- day05/app.py
+++ day05/app.py
@@ -1,41 +1,60 @@
 #!/usr/bin/env python
 
-import pytest
-
 
 def read_input():
-    file_data = open('test_data.txt', mode='r', encoding='utf-8')
-    return file_data
+    with open('test_data.txt', mode='r', encoding='utf-8') as file:
+        return file.readlines()
 
 
-def parse_stack_headers(input_data):
+def create_stacks(input_data):
+    result = []
+
+    list_of_stacks = []
     for line in input_data:
         if line != '\n':
-            previous_line = line.strip()
-        elif line == '\n':
+            list_of_stacks.append([x for x in list(line[1::4])])
+        else:
             break
 
-    stack_headers = list(
-        filter(None, [x.strip(' ') for x in previous_line]))
-
-    result = [int(x) for x in stack_headers]
+    rows_to_columns = [list(x) for x in zip(*list_of_stacks)]
+    for row in rows_to_columns:
+        result.append([x for x in row if x != ' '])
 
     return result
 
 
+def parse_rearrangement_procedures(input_data):
+    procedures = []
+    for line in input_data:
+        if line[0].isalpha():
+            procedures.append([int(x) for x in line.split() if x.isdigit()])
+
+    return procedures
+
+
+def move_crates(stacks, procedures):
+
+    for procedure in procedures:
+        crate_index = stacks[procedure[1]].index(
+            stacks[procedure[1]][procedure[0] - 1])
+        print(crate_index)
+
+        stacks[procedure[2] - 1].insert(
+            0, stacks[procedure[1] - 1].pop(crate_index))
+
+        print(stacks)
+
+
 def main():
     input_data = read_input()
-    parse_stack_headers(input_data=input_data)
+
+    stacks = create_stacks(
+        input_data)
+
+    procedures = parse_rearrangement_procedures(input_data)
+
+    move_crates(stacks, procedures)
 
 
 if __name__ == "__main__":
     main()
-
-
-@pytest.fixture
-def test_data():
-    return read_input()
-
-
-def test_parse_stack_headers(test_data):
-    assert parse_stack_headers(test_data) == ([1, 2, 3])
